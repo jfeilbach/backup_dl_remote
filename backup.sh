2ea5033dbdeb4fb4f7a3489b5ee44f2d21bcb185
jfeilbach <jfeilbach@users.noreply.github.com>
2020-04-04 12:38:25 -0600

    Update backup.sh
    working version

--- backup.sh
+++ backup.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
+# version 1.0
+# 4 April 2020
 
 SECONDS=0
-LOCAL_PATH='/Volumes'
-DIR='vol2'
 
-NC='\e[0m'
-WHITE='\e[1;37m'
-YELLOW='\e[1;33m'
-LIGHT_BLUE='\e[1;34m'
-CYAN='\e[0;36m'
-RED='\e[0;31m'
+stty sane
+
+local_path='/Volumes'
+src='/Volumes/Raid 0/Torrent Downloads/'
+dest='/Volumes/vol2/tinkey_torrents/'
+dir1='vol1'
+dir2='vol2'
+timestamp='/Users/jason/backup.date'
+
+# Mac bash colors
+NC='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+
+get_last () {
+    echo ""
+    time_val=$(cat ${timestamp})
+    echo -e "${WHITE}Last backup was ${NC}${CYAN}${time_val}${NC}\n"
+}
+
+set_last () {
+    rm ${timestamp}
+    $(date > ${timestamp})
+    chown jason:staff ${timestamp}
+}
 
 displaytime () {
    local T=$SECONDS
@@ -24,22 +48,36 @@ displaytime () {
    printf '%d seconds\n' $S
 }
 
-echo "Checking for root privelages..."
-[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-
-echo -e "${CYAN}You are now user:${NC} ${YELLOW}$(whoami)${NC}\n"
-
-/usr/bin/osascript -e "try" -e "mount volume \"smb://jason@nas5.local/vol2/\"" -e "end try"
-/usr/bin/osascript -e "try" -e "mount volume \"smb://jason@nas5.local/vol2/\"" -e "end try"
-
-check_mount_exist () {
-    if [[ -d "${LOCAL_PATH}/${DIR}" ]] ; then
-        echo -e "Mount point ${WHITE}${LOCAL_PATH}/${DIR}${NC} exists. Proceeding...\n"
-        else
-        	echo -e "${RED}Mount point ${WHITE}${LOCAL_PATH}/${DIR}${NC}${RED} does not exist. Exiting.${NC}\n"
-        	exit 1
+check_mount () {
+    if mount | grep "on $1" > /dev/null; then
+        echo -e "${GREEN}Success:${NC} ${WHITE}${1} is mounted.${NC}\n"
+    else
+        echo -e "#{RED}Warning. ${1} is NOT mounted.${NC}${YELLOW} Mounting...${NC}\n"
+        /usr/bin/osascript -e "try" -e "mount volume \"smb://jason@nas5.local/vol1/\"" -e "end try"
+        retVal=$?
+        if [ $retVal -ne 0 ]; then
+            echo -e "${RED}Error: Command failed. Exiting.${NC}\n"
+            exit $retVal
+        fi
     fi
 }
+
+get_last
+
+echo -e "Checking for root priveleges...\n"
+[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
+
+echo -e "${CYAN}You are now the user:${NC} ${YELLOW}$(whoami)${NC}\n"
+
+echo -e "Checking for mounted volumes...\n"
+
+local_mnt1="${local_path}/${dir1}"
+check_mount ${local_mnt1}
+
+local_mnt2="${local_path}/${dir2}"
+check_mount ${local_mnt2}
+
+echo -e "${YELLOW}Starting backup...${NC}"
 
 rsync -avhz --stats --progress \
 --exclude='$RECYCLE.BIN' \
@@ -73,9 +111,11 @@ rsync -avhz --stats --progress \
 --exclude='Thumbs.db' \
 --exclude='.HFS+ Private Directory Data\#015' \
 --exclude='.PKInstallSandboxManager-SystemSoftware' \
-/Volumes/Raid\ 0/Torrent\ Downloads/ /Volumes/vol2/tinkey_torrents/
+"${src}" "${dest}"
+
+set_last
 
 echo ""
-echo "Took $(displaytime) to complete ${0}."
+echo -e "${CYAN}Finished.${NC}${GREEN} ${0}${NC}${CYAN} Completed in ${NC}${WHITE}$(displaytime)${NC}.\n"
 
 exit 0
